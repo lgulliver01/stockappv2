daa130015f1da090522b3bf72737ccced17f5286
Luke G <gullivluke@ecs.vuw.ac.nz>
2022-12-06 23:21:25 +1300

    importing more functionality, still lacking/buggy

--- script.js
+++ script.js
@@ -1,57 +1,5 @@
-window.onload = function() {
-
 var data;
-
-if (window.localStorage.getItem('data') == null) {
-    data = JSON.parse(`[
-    {
-        "ticker": "AAPL",
-        "shares": 10,
-        "averagePx": 110.25,
-        "lastPx": 110.25,
-        "PL": 0
-        
-    },
-    {
-        "ticker": "AMZN",
-        "shares": 10,
-        "averagePx": 100.00,
-        "lastPx": 100.00,
-        "PL": 0
-    }
-]`);
-
-} else {
-    data = JSON.parse(window.localStorage.getItem('data'));
-}
-
 var ovrPL;
-if (window.localStorage.getItem('ovrPL') == undefined) {
-    ovrPL = 0;
-
-} else {
-    ovrPL = window.localStorage.getItem('ovrPL');
-}
-
-console.log(data);
-console.log(ovrPL);
-
-save();
-
-drawPL();
-drawTable();
-
-function save() {
-    window.localStorage.setItem("data", JSON.stringify(data));
-    window.localStorage.setItem("ovrPL", ovrPL);
-}
-
-function drawPL() {
-    var plDiv = document.getElementById('plDiv');
-    ovrPL < 0 ? plDiv.style.color = 'red' : plDiv.style.color = 'green';
-    plDiv.innerHTML = `<h3>Overall P/L: ${ovrPL}</h3>`;
-}
-
 
 function drawTable() {
     var tableHTML = "";
@@ -87,7 +35,7 @@ function drawTable() {
                     tableHTML += '<td>' + value2.toLocaleString("en-US", { maximumFractionDigits: 2, minimumFractionDigits: 2 }); + '</td>';
                     break;
             }
-            flag ++;
+            flag++;
         });
         // end
         tableHTML += '</tr>';
@@ -97,4 +45,103 @@ function drawTable() {
     document.getElementById('tableDiv').innerHTML = tableHTML;
 }
 
+function drawPL() {
+    var plDiv = document.getElementById('plDiv');
+    ovrPL < 0 ? plDiv.style.color = 'red' : plDiv.style.color = 'green';
+    plDiv.innerHTML = `<h3>Overall P/L: ${ovrPL}</h3>`;
 }
+
+window.onload = function () {
+    if (window.localStorage.getItem('data') == null) {
+        data = JSON.parse(`[
+    {
+        "ticker": "AAPL",
+        "shares": 10,
+        "averagePx": 110.25,
+        "lastPx": 110.25,
+        "PL": 0
+        
+    },
+    {
+        "ticker": "AMZN",
+        "shares": 10,
+        "averagePx": 100.00,
+        "lastPx": 100.00,
+        "PL": 0
+    }]`
+    );
+
+    } else {
+        data = JSON.parse(window.localStorage.getItem('data'));
+    }
+
+
+    if (window.localStorage.getItem('ovrPL') == undefined) {
+        ovrPL = 0;
+
+    } else {
+        ovrPL = window.localStorage.getItem('ovrPL');
+    }
+
+    console.log(data);
+    console.log(ovrPL);
+
+    drawPL();
+    drawTable();
+}
+
+    function buyOrder() {
+        var ticker = document.getElementById("tickerVal").value.toUpperCase();
+        var shares = document.getElementById("qtyVal").value;
+        var price = document.getElementById("priceVal").value;
+        var found = false;
+        data.forEach(element => {
+            if (element.ticker == ticker) {
+                found = true;
+                var newShares = element.shares + parseInt(shares);
+                var newPrice = (element.averagePx * element.shares + parseInt(shares) * price) / newShares;
+                element.averagePx = newPrice;
+                element.shares = newShares;
+            }
+
+        });
+        if (!found) {
+            var newEntry = {
+                "ticker": ticker,
+                "shares": shares,
+                "averagePx": price,
+                "lastPx": 0,
+                "PL": 0
+            };
+            data.push(newEntry);
+        }
+        save();
+        location.reload();
+    }
+
+    function updatePrices() {
+        data.forEach(element => {
+            getPriceApi(element);
+        });
+
+    }
+
+    function getPriceApi(element) {
+        var tick = element.ticker;
+        var url = 'https://cloud.iexapis.com/stable/stock/' + tick + '/quote?token=pk_b690d064c4d846889899897a577062fe';
+        $.getJSON(url, function (data) {
+            var currPx = data.iexClose;
+            element.lastPx = currPx;
+              console.log(element.ticker + " " + currPx);
+            element.PL = (currPx - element.averagePx) * element.shares;
+            drawTable();
+            save();
+        });
+    }
+
+    function save() {
+        window.localStorage.setItem("data", JSON.stringify(data));
+        window.localStorage.setItem("ovrPL", ovrPL);
+    }
+
+
